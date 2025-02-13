47695a4b427e6eedef5d4d0df8182ee2949cf660
anjumanara28 <anjuanny2024@gmail.com>
2025-02-13 22:38:34 +0000

    added all users page

--- src/Layout/Dashboard/AllUsers.jsx
+++ src/Layout/Dashboard/AllUsers.jsx
@@ -87,7 +87,7 @@ const AllUsers = () => {
                                 <td>
                                     { user.role === 'admin' ? 'Admin' : <button
                                         onClick={() => handleMakeAdmin(user)}
-                                        className="btn btn-lg bg-orange-500">
+                                        className="btn btn-md bg-orange-500">
                                         <FaUsers className="text-white 
                                         text-2xl"></FaUsers>
                                     </button>}
